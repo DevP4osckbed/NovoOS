dab5b4db172d7cb1c01f8e983feb086622a9fedc
DevP4osckbed <devbananastudios@gmail.com>
2025-12-07 14:35:10 +0000

    Cancelled

--- public/app.js
+++ public/app.js
@@ -6,9 +6,32 @@ canvas.height = window.innerHeight;
 
 let running = false;
 
-canvas.addEventListener('mousemove', (event) => {
-    if (!drawing) return;
-});
+ctx.font = '16px Arial';
+ctx.fillStyle = '#000000';
+
+animate();
+
+let last = performance.now();
+let fps = 0;
+let frameCount = 0;
+let fpsTimer = 0;
+
+function animate() {
+    const now = performance.now();
+    frameCount++;
+    fpsTimer += now - last;
+    
+    if (fpsTimer >= 1000) {
+        fps = frameCount;
+        frameCount = 0;
+        fpsTimer = 0;
+    }
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillText("FPS: " + fps, 25, 25);
+    last = now;
+    requestAnimationFrame(animate);
+}
 
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
